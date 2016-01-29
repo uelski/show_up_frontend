3abeb28eac601959dd061bdf3d90338610c6ab5f
Sam Vredenburgh <sam.vburgh@gmail.com>
2016-01-29 00:04:36 -0600

    deploying

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,7 +1,7 @@
 var app = app || {};
 
 $.ajaxPrefilter( function( options, originalOptions, jqXHR ) {
-    options.url = 'http://localhost:3000' + options.url;
+    options.url = 'http://162.243.46.169' + options.url;
   });
 
   // Store "old" sync function
